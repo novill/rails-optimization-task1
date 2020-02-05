37e6315a029c6c31107b2028c9b3888830b50251
Alexey Shemyakin <2alexeysh@gmail.com>
2020-02-05 23:02:29 +0300

    rubocoped

--- work.rb
+++ work.rb
@@ -1,5 +1,3 @@
-# Deoptimized version of homework task
-
 require 'json'
 require 'pry'
 require 'date'
@@ -18,7 +16,7 @@ def parse_user(fields)
       'id' => fields[1],
       'first_name' => fields[2],
       'last_name' => fields[3],
-      'age' => fields[4],
+      'age' => fields[4]
   }
 end
 
@@ -28,40 +26,40 @@ def parse_session(fields)
       'session_id' => fields[2],
       'browser' => fields[3],
       'time' => fields[4],
-      'date' => fields[5],
+      'date' => fields[5]
   }
 end
 
 def load_objects(source_data_file)
-  grouped_file_lines = File.read(source_data_file).split("\n").map{ |s| s.split(',') }.group_by{ |line| line[0] }
+  grouped_file_lines = File.read(source_data_file).split("\n").map { |s| s.split(',') }.group_by { |line| line[0] }
 
-  sessions = grouped_file_lines['session'].map{|fields| parse_session(fields) }
+  sessions = grouped_file_lines['session'].map { |fields| parse_session(fields) }
 
-  grouped_sessions = sessions.group_by{ |s| s['user_id'] }
+  grouped_sessions = sessions.group_by { |s| s['user_id'] }
 
-   [ grouped_file_lines['user'].
-      map{ |fields| parse_user(fields) }.
-      map{ |user| User.new(attributes: user, sessions: grouped_sessions[user['id']]) },
-   sessions]
+  [ grouped_file_lines['user'].
+      map { |fields| parse_user(fields) }.
+      map { |user| User.new(attributes: user, sessions: grouped_sessions[user['id']]) },
+    sessions]
 end
 
 def get_user_stats(users_objects)
-  users_objects.map { |user|
-    prep_sessions = user.sessions.map{ |s| [s['time'].to_i, s['browser'].upcase, s['date'] ] }
+  users_objects.map do |user|
+    prep_sessions = user.sessions.map { |s| [s['time'].to_i, s['browser'].upcase, s['date'] ] }
 
     [
-        "#{user.attributes['first_name']} #{user.attributes['last_name']}",
-        { 'sessionsCount' => user.sessions.size,
-          'totalTime' => prep_sessions.sum(&:first).to_s + ' min.',
-          'longestSession' => prep_sessions.max{ |a, b| a[0] <=> b[0] }[0].to_s + ' min.',
-          'browsers' => prep_sessions.map {|ps| ps[1]}.sort.join(', '),
-          'usedIE' => prep_sessions.any? { |b| b[1].start_with?('INTERNET EXPLORER') },
+      "#{user.attributes['first_name']} #{user.attributes['last_name']}",
+      { 'sessionsCount' => user.sessions.size,
+        'totalTime' => prep_sessions.sum(&:first).to_s + ' min.',
+        'longestSession' => prep_sessions.max { |a, b| a[0] <=> b[0] }[0].to_s + ' min.',
+        'browsers' => prep_sessions.map { |ps| ps[1] }.sort.join(', '),
+        'usedIE' => prep_sessions.any? { |b| b[1].start_with?('INTERNET EXPLORER') },
 
-          'alwaysUsedChrome' => prep_sessions.all? { |b| b[1].start_with?('CHROME') },
-          'dates' => prep_sessions.map {|d| d[2]}.sort.reverse
-        }
+        'alwaysUsedChrome' => prep_sessions.all? { |b| b[1].start_with?('CHROME') },
+        'dates' => prep_sessions.map { |d| d[2] }.sort.reverse
+      }
     ]
-  }
+  end
 end
 
 def work(source_data_file = 'data.txt', disable_gc = false)
@@ -91,7 +89,7 @@ def work(source_data_file = 'data.txt', disable_gc = false)
   report[:totalUsers] = users_objects.size
 
   # Подсчёт количества уникальных браузеров
-  uniqueBrowsers = sessions.map{ |session| session['browser'] }.uniq
+  uniqueBrowsers = sessions.map { |session| session['browser'] }.uniq
 
   report['uniqueBrowsersCount'] = uniqueBrowsers.count
 
