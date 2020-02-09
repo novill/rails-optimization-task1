17f2156c5a57e85bfbea4611dcc53e5f36252954
Alexey Shemyakin <2alexeysh@gmail.com>
2020-02-09 13:57:42 +0300

    perfomance test for large file

--- benchmark_realtime.rb
+++ benchmark_realtime.rb
@@ -1,12 +1,12 @@
 require 'benchmark'
-require_relative 'work2'
+require_relative 'work'
 
 #[100,1000,10000,20000].each do |lines|
 # [1000,2000,4000,8000, 16000].each do |lines|
-['500000'].each do |lines|
+['_large'].each do |lines|
   # puts 'warming'
   # work("data#{lines}.txt") # прогрев
   # GC.start
-  time = Benchmark.realtime { work("data#{lines}.txt", false) }
+  time = Benchmark.realtime { work("data#{lines}.txt", true) }
   puts "Finish #{lines} lines in #{time.round(5)}"
 end
